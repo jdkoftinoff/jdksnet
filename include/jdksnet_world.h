d719c5d0e1e828ca08cc4b4948e610e81aac825c
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-08-19 15:39:08 -0700

    raw packet send/receive code added for macosx win32 linux

---
+++ include/jdksnet_world.h
@@ -0,0 +1,128 @@
+#pragma once
+/*
+Copyright (c) 2014, J.D. Koftinoff Software, Ltd.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#if defined( __linux__ )
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <sys/poll.h>
+#include <errno.h>
+#include <strings.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <linux/sockios.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#elif defined( __APPLE__ )
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <sys/poll.h>
+#include <errno.h>
+#include <strings.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <net/if_dl.h>
+#include <pcap.h>
+#elif defined( _WIN32 )
+#ifndef _WINSOCKAPI_
+#define _WINSOCKAPI_
+#endif
+#include <Windows.h>
+#include <iphlpapi.h>
+#include <winsock2.h>
+#include "jdksavdecc_ms.h"
+#include <pcap.h>
+#pragma comment( lib, "IPHLPAPI.lib" )
+#pragma comment( lib, "wpcap.lib" )
+#pragma comment( lib, "Ws2_32.lib" )
+static inline void bzero( void *buf, size_t sz )
+{
+    memset( buf, 0, sz );
+}
+#endif
+
+#include "jdksavdecc.h"
+#include "jdksavdecc_util.h"
+#include "jdksavdecc_frame.h"
+
+#ifndef JDKSNET_ERROR_STDERR
+#define JDKSNET_ERROR_STDERR 1
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef JDKSNET_ERROR_PROC
+extern void JDKSNET_ERROR_PROC( fmt, ... );
+#define jdksnet_error( ... ) JDKSNET_ERROR_PROC( __VA_ARGS__ )
+#else
+#if JDKSNET_ERROR_STDERR == 1
+#define jdksnet_error( ... ) fprintf( stderr, __VA_ARGS__ )
+#else
+#define jdksnet_error( ... )
+#endif
+#endif
+
+bool jdksnet_init( void );
+
+#ifdef __cplusplus
+}
+#endif
--- src/jdksnet.c
+++ src/jdksnet.c
@@ -1,2 +1,37 @@
 #include "jdksnet_world.h"
 #include "jdksnet.h"
+
+static bool jdksnet_initted = false;
+
+bool jdksnet_init( void )
+{
+    if ( !jdksnet_initted )
+    {
+#if defined( _WIN32 )
+        WSADATA wsaData;
+        WORD version;
+        int error;
+        version = MAKEWORD( 2, 2 );
+        error = WSAStartup( version, &wsaData );
+        if ( error != 0 )
+        {
+            return false;
+        }
+        if ( version != wsaData.wVersion )
+        {
+            return false;
+        }
+        jdksnet_initted = true;
+        return true;
+#elif defined( __linux__ ) || defined( __APPLE__ )
+        struct sigaction act;
+        act.sa_handler = SIG_IGN;
+        sigemptyset( &act.sa_mask );
+        act.sa_flags = 0;
+        sigaction( SIGPIPE, &act, NULL );
+        jdksnet_initted = true;
+        return true;
+#endif
+    }
+    return jdksnet_initted;
+}
